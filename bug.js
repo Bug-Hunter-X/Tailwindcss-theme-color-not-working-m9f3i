03e9921d63011280ed070faf1a4cdee176bec007
BugHunter-X <askzenai@gmail.com>
2025-02-07 01:02:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,14 @@
+```javascript
+//tailwind.config.js
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'theme-color-primary': '#e63946',
+      }
+    }
+  },
+  plugins: [],
+}
+```
